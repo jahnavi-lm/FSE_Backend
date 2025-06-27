b2b66634cc490d53b8e172885eab0265b3aef97b
Jahnavi Lakshmi Muttireddy <jahnavi.muttireddy@bounteous.com>
2025-06-27 11:55:57 +0530

    backend setup

---
+++ src/main/java/com/fse/FSE_Backend_Proj/FseBackendProjApplication.java
@@ -0,0 +1,13 @@
+package com.fse.FSE_Backend_Proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FseBackendProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FseBackendProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fse/FSE_Backend_Proj/FseBackendProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fse.FSE_Backend_Proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FseBackendProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
