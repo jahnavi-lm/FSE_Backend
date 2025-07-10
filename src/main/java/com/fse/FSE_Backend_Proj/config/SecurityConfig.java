2bfa03d9d6980a55ae241fb0367cb3a37ce78bdc
Jahnavi Lakshmi Muttireddy <jahnavi.muttireddy@bounteous.com>
2025-07-10 21:43:24 +0530

    port change

--- src/main/java/com/fse/FSE_Backend_Proj/config/SecurityConfig.java
+++ src/main/java/com/fse/FSE_Backend_Proj/config/SecurityConfig.java
@@ -137,7 +137,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5174")); // Adjust if needed
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // Adjust if needed
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
