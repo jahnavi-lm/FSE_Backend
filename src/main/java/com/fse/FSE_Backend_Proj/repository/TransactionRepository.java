4a3e98a0e8202045c3efce0e80c8e53d3333b33a
ManavBountAcko <manav.shah@bounteous.com>
2025-07-10 15:21:06 +0530

    Updated txn

--- src/main/java/com/fse/FSE_Backend_Proj/repository/TransactionRepository.java
+++ src/main/java/com/fse/FSE_Backend_Proj/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.fse.FSE_Backend_Proj.repository;
 
 import com.fse.FSE_Backend_Proj.model.Transaction;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,9 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+
     List<Transaction> findByInvestorId(String investorId);
+
+    @EntityGraph(attributePaths = {"fundScheme"}) // <-- Must be inside the interface
+    List<Transaction> findByInvestorIdAndFundSchemeId(String investorId, String schemeId); // <-- Move it here
 }
